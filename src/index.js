059129a02f3e25c5cc9a9c756a0cb7b7801c927a
Jaydeep Solanki <jaydp17@gmail.com>
2018-02-23 23:44:51 +0530

    add eslint :rotating_light:

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ const restify = require('restify');
 
 const server = restify.createServer({
   name: 'music-player-backend',
-  version: '1.0.0'
+  version: '1.0.0',
 });
 
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-server.get('/echo/:name', function (req, res, next) {
+server.get('/echo/:name', (req, res, next) => {
   res.send(req.params);
   return next();
 });
 
-server.listen(8080, function () {
+server.listen(8080, () => {
   console.log('%s listening at %s', server.name, server.url);
 });
