190b26327d0cc5a2dd10d11c0d3e641c8a55cacf
Jaydeep Solanki <jaydp17@gmail.com>
2018-02-24 21:52:23 +0530

    refactor a little bit

--- src/channels-string.js
+++ src/channels-string.js
@@ -2,6 +2,11 @@
  * This file just stores the constants
  */
 module.exports = {
+  CONNECTION_CHANNEL: 'connection',
+  DISCONNECT_CHANNEL: 'disconnect',
+
   LISTENER_CHANGE_CHANNEL: 'listeners-change',
   ALL_LISTEN_COUNT_CHANNEL: 'all-listen-count',
+  START_LISTENING_CHANNEL: 'start-listening',
+  STOP_LISTENING_CHANNEL: 'stop-listening',
 };
--- src/socket-handlers.js
+++ src/socket-handlers.js
@@ -1,30 +1,30 @@
 const redis = require('./redis');
-const { LISTENER_CHANGE_CHANNEL, ALL_LISTEN_COUNT_CHANNEL } = require('./channels-string');
+const {
+  CONNECTION_CHANNEL,
+  DISCONNECT_CHANNEL,
+  LISTENER_CHANGE_CHANNEL,
+  ALL_LISTEN_COUNT_CHANNEL,
+  START_LISTENING_CHANNEL,
+  STOP_LISTENING_CHANNEL,
+} = require('./channels-string');
 
 module.exports = io => {
-  io.on('connection', async client => {
-    console.log('a user connected');
-    client.on('start-listening', songId => {
-      console.log(`${client.id} started listening to ${songId}`);
-      redis.startedListening(client.id, songId);
-    });
-    client.on(`stop-listening`, songId => {
-      console.log(`${client.id} stopped listening to ${songId}`);
-      redis.stoppedListening(client.id, songId);
-    });
-    client.on('disconnect', () => {
-      console.log(`${client.id} disconnected`);
-      redis.stoppedListening(client.id, null);
-    });
+  io.on(CONNECTION_CHANNEL, async client => {
+    client.on(START_LISTENING_CHANNEL, songId => redis.startedListening(client.id, songId));
+    client.on(STOP_LISTENING_CHANNEL, songId => redis.stoppedListening(client.id, songId));
+    client.on(DISCONNECT_CHANNEL, () => redis.stoppedListening(client.id, null));
     const result = await redis.getAllListenCount();
     client.emit(ALL_LISTEN_COUNT_CHANNEL, result);
   });
 
   redis.onListenersChange((err, { songId, listenerCount }) => {
     if (err) {
+      // eslint-disable-next-line no-console
       console.error('Error', err);
       return;
     }
-    io.emit(LISTENER_CHANGE_CHANNEL, { songId, listenerCount });
+    if (songId && listenerCount) {
+      io.emit(LISTENER_CHANGE_CHANNEL, { songId, listenerCount });
+    }
   });
 };
