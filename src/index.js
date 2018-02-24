70a1e61015b9ed27e74a584611be34cb7c807c6e
Jaydeep Solanki <jaydp17@gmail.com>
2018-02-24 13:49:11 +0530

    add cors middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const restify = require('restify');
 const restErrors = require('restify-errors');
+const corsMiddleware = require('restify-cors-middleware');
+
 const s3Utils = require('./s3-utils');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const server = restify.createServer({
   name: 'music-player-backend',
   version: '1.0.0',
 });
 
+const origins = ['https://music-player.jaydp.com'];
+if (!isProd) {
+  origins.push('http://localhost:3000');
+}
+const cors = corsMiddleware({ origins });
+
+server.pre(cors.preflight);
+server.use(cors.actual);
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
