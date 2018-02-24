bc6ddbff0d404039e7b29dd6f531fdda2527260e
Jaydeep Solanki <jaydp17@gmail.com>
2018-02-24 19:04:21 +0530

    add socket.io :zap:

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const restify = require('restify');
+const socketio = require('socket.io');
 const applyMiddlewares = require('./apply-middlewares');
 const addRoutes = require('./routes');
 
@@ -6,8 +7,15 @@ const server = restify.createServer({
   name: 'music-player-backend',
   version: '1.0.0',
 });
+const io = socketio.listen(server.server);
+
 applyMiddlewares(server);
 addRoutes(server);
+
+io.on('connection', socket => {
+  console.log('a user connected');
+  socket.broadcast.emit('listeners-change', { songId: 'hello', listeners: 10 });
+});
 
 server.listen(8080, () => {
   console.log('%s listening at %s', server.name, server.url);
